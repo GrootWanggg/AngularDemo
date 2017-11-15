a6fb1922e29ae9982ea1ae648709cead22d05471
LoveWmd <wslawmd1314@Gmail.com>
2017-11-15 17:12:50 +0800

    form表单的一些破判断提示，UI

---
+++ src/app/Form/LoginType.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by 王石磊 on 2017/11/15.
+ */
+export class Login {
+
+  constructor(
+    public id: number,
+    public name: string,
+    public power: string,
+    public alterEgo?: string
+  ) {  }
+
+}
